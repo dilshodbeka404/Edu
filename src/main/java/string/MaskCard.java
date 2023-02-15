ee216f283ba51c193bb59307b68a767eb4c89033
dilshodbek <dilshodbeka404@gmail.com>
2023-02-15 09:51:27 +0500

    update mask card method

--- src/main/java/string/MaskCard.java
+++ src/main/java/string/MaskCard.java
@@ -7,7 +7,16 @@ import java.util.StringTokenizer;
  */
 public class MaskCard {
     public static void main(String[] args) {
-        System.out.println("maskCardholder(\"Dilshodbek Axmedov\") = " + maskCardholder("Dilshodbek Axmedov"));
+        System.out.println(maskCardholder("Dilshodbek Axmedov Kamiljan"));
+
+        String str = "8600021041783708";
+        System.out.println(maskString(str));
+    }
+
+    private static String maskString(String strText) {
+        int maskLength = 6;
+        String sbMaskString = "*".repeat(maskLength);
+        return strText.substring(0, 6) + sbMaskString + strText.substring(6 + maskLength);
     }
 
     public static String maskCardholder(String cardholder) {
@@ -15,7 +24,7 @@ public class MaskCard {
         StringBuilder maskHolderName = new StringBuilder();
         while (holderNames.hasMoreTokens()) {
             String token = holderNames.nextToken();
-            maskHolderName.append(token.charAt(0)).append("*****").append(token.charAt(token.length() - 1)).append(" ");
+            maskHolderName.append(token.charAt(0)).append("******").append(token.charAt(token.length() - 1)).append(" ");
         }
         return maskHolderName.toString();
     }
