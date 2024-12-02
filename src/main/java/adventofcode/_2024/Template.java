e620b8d18f3e89c4d3dc75f7b5a715e49b9068eb
Axmedov Dilshodbek <dilshodbeka404@gmail.com>
2024-12-02 15:14:43 +0500

    optimized

--- src/main/java/adventofcode/_2024/Template.java
+++ src/main/java/adventofcode/_2024/Template.java
@@ -1,13 +1,12 @@
 package adventofcode._2024;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Template {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         System.out.println(">>>>>>>>>>>>>>>> Start file read");
         try (Scanner scan = new Scanner(new File("src/main/resources/input.txt"))) {
             while (scan.hasNextLine()) {
