1387cb2efbf6fce14d7a362feb2e43f725106288
Axmedov Dilshodbek <dilshodbeka404@gmail.com>
2024-12-03 10:26:05 +0500

    add adventofcode 3 day

---
+++ src/main/java/adventofcode/_2024/Day3.java
@@ -0,0 +1,31 @@
+package adventofcode._2024;
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day3 {
+
+    static long sum = 0;
+
+    public static void main(String[] args) {
+        System.out.println(">>>>>>>>>>>>>>>> Start file read");
+        try (Scanner scan = new Scanner(new File("src/main/resources/input.txt"))) {
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                findMul(line);
+            }
+            System.out.println("sum = " + sum);
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+    }
+
+    static void findMul(String text) {
+        Matcher matcher = Pattern.compile("mul\\((-?\\d+),(-?\\d+)\\)").matcher(text);
+        while (matcher.find()) {
+            sum += Long.parseLong(matcher.group(1)) * Long.parseLong(matcher.group(2));
+        }
+    }
+}
