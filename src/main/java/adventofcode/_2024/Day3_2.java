50c0f5a19c31aa57da47849471b669670f678aa7
Axmedov Dilshodbek <dilshodbeka404@gmail.com>
2024-12-03 10:33:06 +0500

    adventofcode - added day 3 part 2

--- src/main/java/adventofcode/_2024/Day3_2.java
+++ src/main/java/adventofcode/_2024/Day3_2.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Day3 {
+public class Day3_2 {
 
     static long sum = 0;
+    static boolean isMulEnabled = true;
 
     public static void main(String[] args) {
         System.out.println(">>>>>>>>>>>>>>>> Start file read");
@@ -23,9 +24,17 @@ public class Day3 {
     }
 
     static void findMul(String text) {
-        Matcher matcher = Pattern.compile("mul\\((-?\\d+),(-?\\d+)\\)").matcher(text);
+        Matcher matcher = Pattern.compile("mul\\((-?\\d+),(-?\\d+)\\)|do\\(\\)|don't\\(\\)").matcher(text);
         while (matcher.find()) {
-            sum += Long.parseLong(matcher.group(1)) * Long.parseLong(matcher.group(2));
+            String match = matcher.group();
+
+            if (match.equals("do()")) {
+                isMulEnabled = true; // Enable mul instructions
+            } else if (match.equals("don't()")) {
+                isMulEnabled = false; // Disable mul instructions
+            } else if (match.startsWith("mul") && isMulEnabled) {
+                sum += Long.parseLong(matcher.group(1)) * Long.parseLong(matcher.group(2));
+            }
         }
     }
 }
